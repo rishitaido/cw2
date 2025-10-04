9a3dd85beb4de5f46b7ce54e0be39f8677f3693e
rishitaido <rishitaido@gmail.com>
2025-10-04 10:56:50 -0400

    RecipesHomeScreen

---
+++ lib/recipe.dart
@@ -0,0 +1,13 @@
+class Recipe {
+  String name;
+  String description;
+  List<String> ingredients;
+  List<String> instructions;
+
+  Recipe({
+    required this.name,
+    required this.description,
+    required this.ingredients,
+    required this.instructions,
+  });
+}
